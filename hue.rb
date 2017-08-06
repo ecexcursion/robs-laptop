3059fc4c2bd2dfb8e5535c9dc8d16de77cbcd335
John Gerpheide <Rob@Johns-MacBook-Pro.local>
2017-08-05 19:55:14 -0500

    added the ability to pass in command line args

--- hue.rb
+++ hue.rb
@@ -12,28 +12,43 @@ require 'yaml'
 # https://stackoverflow.com/questions/2024805/ruby-send-json-request
 # https://stackoverflow.com/questions/11403728/how-can-i-send-an-http-put-request-in-ruby
 
-yaml_options = YAML.load_file('huecreds.yaml')
-apikey = yaml_options['hue_apikey']
+def generate_url(bulb)
+  baseuri = 'http://192.168.1.4:80/'
+  load_yaml = YAML.load_file('huecreds.yaml')
+  apikey = load_yaml['hue_apikey']
+  path = "api/#{apikey}/lights/#{bulb}/state"
+  joined = baseuri + path
+  puts apikey
+  puts joined
+  URI(joined)
+end
 
-baseuri = 'http://192.168.1.4:80/'
-path = "api/#{apikey}/lights/1/state"
-joined = baseuri + path
-@uri_symbol = URI(joined)
-
-puts apikey
-
-on = {on: true}.to_json
-off = {on: false}.to_json
-
-
-def light1(lightstate)
-  uri = @uri_symbol
+def lights(bulb, lightstate)
+  uri = generate_url(bulb)
   req = Net::HTTP::Put.new(uri, 'Content-Type' => 'application/json')
   req.body = lightstate
-  puts req.body
+  puts "bulb " + bulb + " " + req.body
   res = Net::HTTP.start(uri.hostname, uri.port) do |http|
     http.request(req)
   end
 end
 
-light1(on)
+# define json body for on/off function
+on = {on: true}.to_json
+off = {on: false}.to_json
+
+# Get user input passed in from console
+#   format is "ruby scriptname light_number state"
+#   example: "ruby hue.rb 1 on"
+
+light_num = ARGV[0]
+light_state = ARGV[1]
+
+if light_state == 'on'
+  lights(light_num, on)
+end
+if light_state == 'off'
+  lights(light_num, off)
+end
+
+# lights('1', on)
